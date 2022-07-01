f5a7bdec039895c5014f89db5c35d6bca6a8df8d
dharmarajmangrola <84985746+dharmarajmangrola@users.noreply.github.com>
2022-07-01 10:11:31 +0530

    Add files via upload

---
+++ equal_number.py
@@ -0,0 +1,10 @@
+#Accept two Integer sand check if they are equal.
+
+num1 = input("Enter Number1 :- ")
+num2 = input("Enter Number2 :- ")
+
+if num1 == num2:
+    print("NUM1 AND NUM2 ARE EQUAL")
+
+else:
+    print("NUM1 AND NUM2 ARE NOT EQUAL")
