b01c271132fc65c1786bf2689e2b05ff59261ff6
dharmarajmangrola <84985746+dharmarajmangrola@users.noreply.github.com>
2022-07-01 09:23:34 +0530

    Add files via upload

---
+++ natural_number.py
@@ -0,0 +1,8 @@
+#To find remainder of division operation where the dividend and divisor are both natural number.
+
+dividend = int(input("Enter dividend :- "))
+divisor = int(input("Enter divisor :- "))
+
+remainder = dividend % divisor
+
+print ("reamainder : " + str(remainder))
