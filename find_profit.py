4fd9dc4cc8469e237e8425533aff68efd2245656
dharmarajmangrola <84985746+dharmarajmangrola@users.noreply.github.com>
2022-07-01 10:07:22 +0530

    Add files via upload

---
+++ find_profit.py
@@ -0,0 +1,9 @@
+# To find profit based on cost price and sell price of an item.
+
+cs = int(input("Enter cost price :-"))
+se = int(input("Enter sell price :-"))
+ 
+if cs > se:
+    print ("You are loss")
+else:
+    print ("you are in profit")
