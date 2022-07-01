b7e3646f25075cfc24d8498e508f70aa2f720c6a
dharmarajmangrola <84985746+dharmarajmangrola@users.noreply.github.com>
2022-07-01 12:25:37 +0530

    Add files via upload

---
+++ three_number.py
@@ -0,0 +1,14 @@
+# To find maximum from the given three number.
+
+number1 = int(input("Enter first number :-"))
+number2 = int(input("Enter second number :-"))
+number3 = int(input("Enter third number :-"))
+
+if number1 == number2 and number3:
+    print("All number are equals")
+else:
+    print("number are not equal")
+
+
+
+
