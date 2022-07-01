5db99f5954ca7b40417a38ee30ba43fc7272b67c
dharmarajmangrola <84985746+dharmarajmangrola@users.noreply.github.com>
2022-07-01 09:47:45 +0530

    Add files via upload

---
+++ volume_of_sphere.py
@@ -0,0 +1,7 @@
+#To find volume of sphere. Formula V=(4πr^3)/3 where π=3.14 and r is radius of sphere.
+
+r = int(input("Enter radius of string :- "))
+
+v = ( 4 * 3.14 * r * r * r) / 3
+
+print ("Enter volum of string :- " + str(v))
