e37feaa1e4135aa3b10c21a680c7fafc37d993cc
dharmarajmangrola <84985746+dharmarajmangrola@users.noreply.github.com>
2022-07-01 10:27:49 +0530

    Add files via upload

---
+++ maximum.py
@@ -0,0 +1,9 @@
+#To find maximum from the given two number.
+
+number1 = int(input("Enter number one :-"))
+number2 = int(input("Enter number secound :-"))
+
+if number1 > number2:
+    print ("number1 is maximum number ")
+else:
+    print ("number2 is maximum number ")
